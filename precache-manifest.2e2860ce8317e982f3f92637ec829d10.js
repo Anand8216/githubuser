969ab4220ca3597aa2a22f2d8011ab8616aaa506
Anand8216 <samaysodhiya304@gmail.com>
2022-09-30 20:25:37 +0530

    Updates

---
+++ precache-manifest.2e2860ce8317e982f3f92637ec829d10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e5362da20d90e5912229067d68ac11",
+    "url": "/githubuser/index.html"
+  },
+  {
+    "revision": "196fd77ab7943020a679",
+    "url": "/githubuser/static/css/main.ff45c5cb.chunk.css"
+  },
+  {
+    "revision": "7717370a95aca2a5cc86",
+    "url": "/githubuser/static/js/2.c48118c5.chunk.js"
+  },
+  {
+    "revision": "fa46e96c2480c2a07a5b1aac4bf0fa5c",
+    "url": "/githubuser/static/js/2.c48118c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196fd77ab7943020a679",
+    "url": "/githubuser/static/js/main.cbc3a6f1.chunk.js"
+  },
+  {
+    "revision": "694e92bb69851984dd93",
+    "url": "/githubuser/static/js/runtime-main.5504ba48.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/githubuser/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/githubuser/static/media/preloader.c114fda3.gif"
+  }
+]);
